fb9da52f8df2e723cfd70d961eab06fe7f1af549
Haysansan <haysan.eang@student.cadt.edu.kh>
2025-12-26 13:58:36 +0700

    Completions

--- lib/START (4)/main.dart
+++ lib/START (4)/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 // import 'ui/groceries/grocery_list.dart';
-import 'ui/groceries/tab/grocery_tab.dart';
+import 'ui/groceries/tab/grocery_tab.dart'; ///Using grocery tab instead of list ui
 
 
 void main() {
